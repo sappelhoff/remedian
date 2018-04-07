d22454537513926a899fe9e7493e544feb39b742
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-04-07 13:25:51 +0200

    use first actual test. more to come

--- test/test_remedian.py
+++ test/test_remedian.py
@@ -1,2 +1,16 @@
-def test_nothing():
-    assert True
+"""Tests for the Remedian class."""
+
+from remedian.remedian import Remedian
+import numpy as np
+
+
+def test_initial_values():
+    """Test the initial values."""
+    random_shape = np.random.randint(1, 100, np.random.randint(1, 5, 1)[0])
+    obs_size = np.random.random((random_shape)).shape
+    n_obs = np.random.randint(0, 100, 1)[0]
+    t = np.random.randint(0, 100, 1)[0]
+    r = Remedian(obs_size, n_obs, t)
+    assert r.obs_size == list(obs_size)
+    assert r.n_obs == n_obs
+    assert r.t == t
