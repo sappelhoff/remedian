2131f7b79303be56edb53e3f74aedef7de429d0f
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-04-17 21:19:09 +0200

    using io.open to allow for encoding param in python 2

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 """Setup remedian."""
 from setuptools import setup, find_packages
 from os import path
+import io
 
 here = path.abspath(path.dirname(__file__))
 
 # Get long description from README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with io.open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='remedian',
@@ -24,7 +25,7 @@ setup(name='remedian',
       keywords='remedian median memory efficient big data',
       packages=find_packages(),
       install_requires=['numpy>=1.14.1'],
-      python_requires='>=2.7, >=3.6',
+      python_requires='>=2.7',
       extras_require={
         'test': ['nose>=1.3.7']
       },
