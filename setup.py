04e92758a72f1a9690e6b662a80308caed78f0cb
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-04-17 20:59:44 +0200

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
 """Setup remedian."""
-from setuptools import setup
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get long description from README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='remedian',
-      version='0.0.1',
+      version='0.1.0',
       description='Remedian: robust averaging of large data sets',
+      long_description=long_description,
       url='http://github.com/sappelhoff/remedian',
       author='Stefan Appelhoff',
       author_email='stefan.appelhoff@mailbox.org',
-      license='MIT',
-      packages=['remedian'],
-      zip_safe=False)
+      classifiers=[
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Science/Research'
+      ],
+      keywords='remedian median memory efficient big data',
+      packages=find_packages(),
+      install_requires=['numpy'],
+      extras_require={
+        'test': ['nose']
+      },
+      project_urls={
+        'Bug Reports': 'https://github.com/sappelhoff/remedian/issues',
+        'Source': 'https://github.com/sappelhoff/remedian'
+      })
