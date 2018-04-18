ba08711891190ca6b51d3ea03c78524bd4e12c68
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-04-18 13:04:58 +0200

    dox

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -24,7 +24,7 @@ copyright = '2018, Stefan Appelhoff'
 author = 'Stefan Appelhoff'
 
 # The short X.Y version
-version = '0.1.0'
+version = ''
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
 
@@ -41,6 +41,7 @@ release = '0.1.0'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -65,7 +66,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -76,7 +77,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,24 +1,14 @@
 .. remedian documentation master file, created by
-   sphinx-quickstart on Wed Apr 18 11:34:33 2018.
+   sphinx-quickstart on Wed Apr 18 12:49:36 2018.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
 Welcome to remedian's documentation!
 ====================================
 
-THIS is some text that I am writingTHIS is some text that I am writing
-THIS is some text that I am writing
-THIS is some text that I am writing
-THIS is some text that I am writing
-THIS is some text that I am writing
-THIS is some text that I am writing
-
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
-
-   modules
-   remedian
 
 
 
