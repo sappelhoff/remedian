88e44873e9825b7723086d41c294102d9eb074bc
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2020-04-01 14:10:40 +0200

    fix tests

--- remedian/tests/test_remedian.py
+++ remedian/tests/test_remedian.py
@@ -31,7 +31,7 @@ def test_initial_values():
     """Test the initial values."""
     random_shape = np.random.randint(1, 100, np.random.randint(1, 5, 1)[0])
     obs_size = np.random.random((random_shape)).shape
-    n_obs = np.random.randint(1, 100, 1)[0]
+    n_obs = np.random.randint(2, 100, 1)[0]
     t = np.random.randint(1, 100, 1)[0]
     r = Remedian(obs_size, n_obs, t)
     assert r.obs_size == list(obs_size)
